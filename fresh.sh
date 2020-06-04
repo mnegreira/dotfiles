23b1d24aec501c6fa8c222b44c4e249e2c446446
Marcos <negr.marcos@gmail.com>
2020-06-03 20:30:21 -0700

    Removed PHP & MySQL settings

--- fresh.sh
+++ fresh.sh
@@ -14,11 +14,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
-# Install PHP extensions with PECL
-pecl install memcached imagick
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer laravel/spark-installer laravel/valet
